05de26743e0b1db5a35b74cae84547b2b386c15b
jakeg00dwin <jakegoodwin@gorge.works>
2023-12-05 00:03:44 -0800

    added a main.rs file with attempts at getting TDD working

---
+++ src/bin/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![feature(type_alias_impl_trait)]
+#![test_runner(my_runner)]
+
+use {defmt_rtt as _, panic_probe as _};
+
+fn my_runner(tests: &[&i32]) {
+    for t in tests {
+        if **t == 0 {
+            //defmt::println!("PASSED");
+        } else {
+            //defmt::println!("FAILED");
+        }
+    }
+}
+
+#[test_case]
+const WILL_PASS: i32 = 0;
+
+#[test_case]
+const WILL_FAIL: i32 = 4;
