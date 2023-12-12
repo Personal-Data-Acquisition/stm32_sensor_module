f6d1d9c8c37ed9670a314b94f4412db22630fef7
jakeg00dwin <jakegoodwin@gorge.works>
2023-12-11 23:00:28 -0800

    changed the unix permissions on the files to allow execution of the scripts

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Author: jake, <Add your name here>
+# Filename: test.sh
+# Descripion: A small script to automate the needed sections to test the project
+
+cargo test --target x86_64-unknown-linux-gnu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
